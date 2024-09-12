4374a6c69f320406b39c8965a39083183f433ebd
Aashish Gurung <watashiaashishgurung@gmail.com>
2024-09-12 10:22:47 +0200

    added browertester localstorage compatability checker

---
+++ toDolist/browsertester/browsertester.js
@@ -0,0 +1,21 @@
+// This function test weather the borwers supports local storage or not
+// To test this function, you can run it in the browser console
+
+// Check if the browser supports localStorage
+function isLocalStorageSupported() {
+    try {
+      const testKey = '__test__';
+      localStorage.setItem(testKey, testKey);
+      localStorage.removeItem(testKey);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  
+  if (isLocalStorageSupported()) {
+    console.log('localStorage is supported in this browser!');
+  } else {
+    console.log('Sorry, localStorage is not supported in this browser.');
+  }
+  
